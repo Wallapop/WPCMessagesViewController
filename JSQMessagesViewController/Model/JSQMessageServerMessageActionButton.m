4468a7b17bd493c9ed27fa0d07745fae30394513
Luis Ascorbe <devlascorbe@gmail.com>
2015-12-22 19:00:27 +0100

    Create JSQMessageServerMessageActionButton class

---
+++ JSQMessagesViewController/Model/JSQMessageServerMessageActionButton.m
@@ -0,0 +1,23 @@
+//
+//  Created by Luis Ascorbe
+//  https://twitter.com/Lascorbe
+//
+//
+//  Documentation
+//  http://cocoadocs.org/docsets/JSQMessagesViewController
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQMessagesViewController
+//
+//
+//  License
+//  Copyright (c) 2014 Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+#import "JSQMessageServerMessageActionButton.h"
+
+@implementation JSQMessageServerMessageActionButton
+
+@end
